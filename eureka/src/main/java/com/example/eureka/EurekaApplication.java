9ef531b54e78c0f8f547ca5f30f39552fbb72776
rexisme <996867328@qq.com>
2018-08-09 16:32:24 +0800

    测试jenkins触发器1

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -10,6 +10,6 @@ public class EurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
-        System.out.println("123.................");
+        System.out.println("123................");
     }
 }
